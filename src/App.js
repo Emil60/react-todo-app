d5d3faf4c89efe47cd36fe3e525cf8b7b059a1fc
Emil <ojagverdiyev13@gmail.com>
2021-08-07 00:53:51 +0400

    class base component created

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+      
+        
+//         <h1>
+//           I edited this App))
+//         </h1>
+        
+      
+//     </div>
+//   );
+// }
+
+class App extends Component{
+  render(){
+    const a = 5;
+    const flag = true;
+    return(
+      <div className="App">
+        <h1>Xello React</h1>
+        <h2> Another h</h2>
+        <h3>{ 5+3 }</h3>
+        <h3>{ 'Emil'.toUpperCase() }</h3>
+        <h3>{ a }</h3>
+        <div>
+          {
+            flag ? <p>Flag is True</p> 
+            : <p>Flag is False</p>
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
